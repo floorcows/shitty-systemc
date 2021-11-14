c1bea1c5190e17c6aeec05a0ffd12b691813c266
robertnakhle <robert.nakhle99@gmail.com>
2021-11-14 01:58:41 +0100

    merge de merde

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "platform.hpp"
 
+
 const string directory = "./";
 
 using namespace std;
@@ -12,42 +13,6 @@ int main() {
     list<string> my_list = file_to_list(directory + "testdata/program.txt");
 
     // for(string str : my_list){ cout<<str<<endl;}
-
-    //---------------------------------------------------
-
-    Cpu *test_cpu = new Cpu();
-    test_cpu->load(directory + "testdata/cpu1.txt");
-    test_cpu->print();
-
-    Bus *test_bus = new Bus();
-    test_bus->load(directory + "testdata/bus1.txt");
-    test_bus->print();
-
-    Memory *test_mem = new Memory();
-    test_mem->load(directory + "testdata/mem1.txt");
-    test_mem->print();
-
-    Display *test_disp = new Display();
-    test_disp->load(directory + "testdata/display.txt");
-    test_disp->print();
-
-    //---------------------------------------------------
-
-    /*Decode an execute all CPU instruction
-        for (string str : my_list) {
-          // list<string> word_list = string_to_words(str);
-          // for(string word : word_list){cout<<word<<"\t";}
-        cout << decode_and_execute(str) << endl;
-          // cout <<endl;
-        }
-    */
-    cout << "\t[\033[31mmain\033[0m]End" << endl;
-    Platform myPlatform("./testdata/platform.txt");
-    myPlatform.load_components();
-    myPlatform.print();
-  }
-  catch (const exception &ex) {
-    cerr << "Exception: '" << ex.what() << "'!" << endl;
 	
 	//---------------------------------------------------
 /*
